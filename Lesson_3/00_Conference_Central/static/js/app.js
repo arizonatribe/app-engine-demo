69df32e749321f632c83b6ec8a32a33163fa217e
David Nunez <arizonatribe@gmail.com>
2016-01-01 23:15:46 -0700

    Added changes from Lesson 3

--- Lesson_3/00_Conference_Central/static/js/app.js
+++ Lesson_3/00_Conference_Central/static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '860836366947-mlvo10kihl013im6sah3dm4rc1b7mp95.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
